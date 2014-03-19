f71cb613731e77760c0cd4437def2afbab85467c
nkowales <nkowales@nd.edu>
2014-03-19 15:11:45 -0400

    Update Enemy.h

--- Enemy.h
+++ Enemy.h
@@ -7,6 +7,7 @@ private:
 	int y;//y position
 	int xv;//x velocity
 	int yv;//y velocity
+	int speed;
 	int maxhealth;
 	int currenthealth;
 
@@ -16,5 +17,19 @@ public:
 	void move();
 	void show();
 	void damage(int);
+	void getx();
+	void gety();
+	void getxv();
+	void getxy();
+	void getSpeed();
+	void getmaxhealth();
+	void get currenthealth();
+	void setx();
+	void sety();
+	void setxv();
+	void setxy();
+	void setSpeed();
+	void setmaxhealth();
+	void setcurrenthealth();
 
 };
